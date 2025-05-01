a8b235f6f032d143a2c9ef5112acf34c460c277a
YullWanamaker <dla4038@naver.com>
2025-05-01 23:21:58 +0900

    fix: Add webpack configuration for motion-utils

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'motion-utils': 'framer-motion',
+    }
+    return config
+  },
   /* config options here */
 };
 
